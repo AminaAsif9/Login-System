3ed81032bc434d6cdc09b412155301dfb83be9e8
Ammy <95443359+AminaAsif9@users.noreply.github.com>
2024-03-05 22:59:36 +0500

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+<!-- All Rights Reserved By https://github.com/AminaAsif9  -->
+<!-- Developed and Designed By https://github.com/AminaAsif9  -->
 <?php
   include("db_con.php"); 
   $error = '';
@@ -19,28 +21,8 @@
             header("location: index.php");
 
     }else{
+            $error = '<p class="text-danger text-center">Invalid Username or Password.</p>';
         
-        if(@$query_fetch['username'] != $username AND  @$query_fetch['password'] == $password){
-            //Incorrect User
-            $error = '<div class="alert alert-danger dark alert-dismissible fade show" role="alert">
-            <strong>Sorry ! </strong> Your username/email is incorrect.
-          <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close" data-bs-original-title="" title=""></button>
-        </div>';
-
-      }elseif(@$query_fetch['username'] == $username AND  @$query_fetch['password'] != $password){
-            // Incorrect Password
-            $error = '<div class="alert alert-danger dark alert-dismissible fade show" role="alert">
-            <strong>Sorry ! </strong> Your password is incorrect.
-          <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close" data-bs-original-title="" title=""></button>
-        </div>';
-        }else{
-
-            // Incorrect Username and Password
-            $error = '<div class="alert alert-danger dark alert-dismissible fade show" role="alert">
-            <strong>Sorry ! </strong> Your username/email and password are incorrect.
-          <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close" data-bs-original-title="" title=""></button>
-        </div>';
-        }
     }
 
 }
@@ -52,23 +34,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
-  <section>
-    <div class="card container ">
-    <!-- <div class="container"> -->
-      <div class="row d-flex justify-content-center text-center">
+  <section class="container">
+   <div class="row d-flex justify-content-center">
         <div class="col-lg-7">
-          <h2 class="fw-bold my-5 ">Login to your Account</h2>
+        <div class="card mt-5 bg-dark text-light">
+  <div class="card-body p-5">
+          <h2 class="fw-bold mb-3 text-center">Login to your Account</h2>
           <?= $error; ?>
 
           <form method="post" action="">
 
-            <!-- Email input -->
+            <!-- Username input -->
             <div class="mb-4">
               <label>Username</label>
-              <input type="text" name="username" class="form-control" placeholder="Enter your Username" required/>
-            </div>
+            <input type="text" name="username" class="form-control" placeholder="Enter your Username" required/>
+</div>
 
             <!-- Password input -->
             <div class="mb-4">
@@ -77,22 +60,23 @@
             </div>
 
             <!-- Submit button -->
+            <div class="text-center">
             <button type="submit" name="login_btn" class="btn btn-primary mb-4">
               Login
             </button>
 
             <!-- Register button -->
-            <div>
               <p>Don't have an account?</p>
-                <a href="signup.php">Signup here</a>
+                <a href="signup.php" class="text-decoration-none">Signup here</a>
             </div>
           </form>
         </div>
       </div>
-      <!-- </div> -->
+      </div>
     </div>
   </section>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+<!-- Developed and Designed By https://github.com/AminaAsif9  -->
